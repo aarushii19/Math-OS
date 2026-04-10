74c5abcd77f797ac7b027cd4841be618daffa17b
Yojitha Boyina <yojithaboyina29@gmail.com>
2026-04-10 18:00:44 +0000

    docs: added demo video of MathOS walkthrough

--- kernel.asm
+++ kernel.asm
@@ -1,10 +1,95 @@
+
+
 [org 0x1000]
 
 mov ah, 0x0e
 
+
 mov si, title
 call print_string
 call newline
+
+mov si, menu
+call print_string
+call newline
+
+mov ah, 0
+int 0x16
+mov bl, al
+
+mov ah, 0x0e
+int 0x10
+call newline
+
+mov si, msg1
+call print_string
+
+mov ah, 0
+int 0x16
+mov cl, al
+
+mov ah, 0x0e
+int 0x10
+sub cl, '0'
+call newline
+
+mov si, msg2
+call print_string
+
+mov ah, 0
+int 0x16
+mov dl, al
+
+mov ah, 0x0e
+int 0x10
+sub dl, '0'
+call newline
+
+cmp bl, '1'
+je add_op
+
+cmp bl, '2'
+je sub_op
+
+cmp bl, '3'
+je mul_op
+
+cmp bl	, '4'
+je div_op
+
+jmp end
+
+add_op:
+add cl, dl
+jmp print_result
+
+sub_op:
+sub cl, dl
+jmp print_result
+
+mul_op:
+mov al, cl
+mul dl
+mov cl, al
+jmp print_result
+
+div_op:
+mov al, cl
+mov ah, 0
+div dl
+mov cl, al
+jmp print_result
+
+print_result:
+add cl, '0'
+
+mov si, result_msg
+call print_string
+
+mov al, cl
+mov ah, 0x0e
+int 0x10
+
 
 print_string:
 lodsb
@@ -25,3 +110,10 @@ ret
 
 end:
 jmp $
+
+
+title db 'MathOS',0
+menu db '1:Add 2:Sub 3:Mul 4:Div',0
+msg1 db 'Enter A: ',0
+msg2 db 'Enter B: ',0
+result_msg db 'Result: ',0
